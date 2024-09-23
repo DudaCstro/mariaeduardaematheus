7d922b48184737b095b7b8d8d57e1b76bba891b9
Maria Eduarda <maria.soares2003@outlook.com>
2024-09-23 18:42:04 -0300

    Adicionando aplicações para cadastro de produto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Pagina from "./componentes/layouts/Pagina";
+import TelaCadastroProduto from "./componentes/Telas/TelaCadastroProduto";
+import TelaCadastroCategoria from "./componentes/Telas/TelaCadastroCategoria";
+import TelaMenu from "./componentes/Telas/TelaMenu";
+import Tela404 from "./componentes/Telas/Tela404";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        { //A ordem das rotas é importante 
+        }
+        <Routes>
+          <Route path="/produto" element={<TelaCadastroProduto />} />
+          <Route path="/categoria" element={<TelaCadastroCategoria />} />
+          <Route path="/" element={<TelaMenu />} />
+          <Route path="*" element={<Tela404 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
