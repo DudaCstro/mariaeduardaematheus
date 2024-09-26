57b3dbd595e45a836285202250f25299512aed60
Matheus-Chizzolini <matchizzolini@gmail.com>
2024-09-26 17:03:42 -0300

    adicionando cadastro de entregador

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Pagina from "./componentes/layouts/Pagina";
 import TelaCadastroProduto from "./componentes/Telas/TelaCadastroProduto";
 import TelaCadastroCategoria from "./componentes/Telas/TelaCadastroCategoria";
 import TelaCadastroFornecedor from "./componentes/Telas/TelaCadastroFornecedor";
+import TelaCadastroEntregador from "./componentes/Telas/TelaCadastroEntregador";
 import TelaMenu from "./componentes/Telas/TelaMenu";
 import Tela404 from "./componentes/Telas/Tela404";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -16,6 +17,7 @@ function App() {
           <Route path="/produto" element={<TelaCadastroProduto />} />
           <Route path="/categoria" element={<TelaCadastroCategoria />} />
           <Route path="/fornecedor" element={<TelaCadastroFornecedor/>} />
+          <Route path="/entregador" element={<TelaCadastroEntregador/>} />
           <Route path="/" element={<TelaMenu />} />
           <Route path="*" element={<Tela404 />} />
         </Routes>
